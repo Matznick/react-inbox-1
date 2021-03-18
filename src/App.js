f0c24ba29de983267f2424b265d531c86711898c
John Doe <christianhillebrand@Fakultat73siMac.fritz.box>
2021-03-18 14:37:47 +0100

    get

--- src/App.js
+++ src/App.js
@@ -5,77 +5,20 @@ import React, { Component } from 'react'
 
 class App extends Component {
   state = {
-    messages: [
-      {
-        id: 1,
-        subject:
-          "You can't input the protocol without calculating the mobile RSS protocol!",
-        read: false,
-        starred: true,
-        labels: ['dev', 'personal']
-      },
-      {
-        id: 2,
-        subject:
-          "connecting the system won't do anything, we need to input the mobile AI panel!",
-        read: false,
-        starred: false,
-        selected: true,
-        labels: []
-      },
-      {
-        id: 3,
-        subject:
-          'Use the 1080p HTTP feed, then you can parse the cross-platform hard drive!',
-        read: false,
-        starred: true,
-        labels: ['dev']
-      },
-      {
-        id: 4,
-        subject: 'We need to program the primary TCP hard drive!',
-        read: true,
-        starred: false,
-        selected: true,
-        labels: []
-      },
-      {
-        id: 5,
-        subject:
-          'If we override the interface, we can get to the HTTP feed through the virtual EXE interface!',
-        read: false,
-        starred: false,
-        labels: ['personal']
-      },
-      {
-        id: 6,
-        subject: 'We need to back up the wireless GB driver!',
-        read: true,
-        starred: true,
-        labels: []
-      },
-      {
-        id: 7,
-        subject: 'We need to index the mobile PCI bus!',
-        read: true,
-        starred: false,
-        labels: ['dev', 'personal']
-      },
-      {
-        id: 8,
-        subject:
-          'If we connect the sensor, we can get to the HDD port through the redundant IB firewall!',
-        read: true,
-        starred: true,
-        labels: []
-      }
-    ],
-    bulkSelectState: 'some',
-    totalUnreadMessageCount: 0
+    messages: [],
+    bulkSelectState: null,
+    totalUnreadMessageCount: null
   }
 
-  componentDidMount () {
+  componentDidMount() {
+    this.getMessagesfromServer()
     this.countAllUnreadMessages()
+  }
+
+  getMessagesfromServer = async () => {
+    const messagesRaw = await fetch('http://localhost:8082/api/messages')
+    const messagesJSON = messagesRaw.json()
+    this.setState({ messages: messagesJSON }, () => console.log(this.state))
   }
 
   countAllUnreadMessages = () => {
@@ -168,7 +111,7 @@ class App extends Component {
     this.setState({ messages: newList })
   }
 
-  render () {
+  render() {
     return (
       <div className="App">
         <Toolbar
@@ -177,9 +120,9 @@ class App extends Component {
           markSelectedAsRead={this.markSelectedAsRead}
           markSelectedAsUnRead={this.markSelectedAsUnRead}
           deleteSelectedMessages={this.deleteSelectedMessages}
-          addSelectedLabel = {this.addSelectedLabel}
-          removeSelectedLabel = {this.removeSelectedLabel}
-          unreadMessageCount = {this.state.totalUnreadMessageCount}
+          addSelectedLabel={this.addSelectedLabel}
+          removeSelectedLabel={this.removeSelectedLabel}
+          unreadMessageCount={this.state.totalUnreadMessageCount}
         />
         <MessageList
           messages={this.state.messages}
